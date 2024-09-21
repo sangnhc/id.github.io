d381224679f1aafa10344dcadbcdd44e27f84021
sangnhc <sangbeau@gmail.com>
2024-09-21 08:55:49 +0700

    Tß╗▒ ─æß╗Öng commit tß╗½ script .bat

--- deployGitlap.bat
+++ deployGitlap.bat
@@ -11,43 +11,11 @@ git add .
 REM Commit các thay đổi với thông điệp
 git commit -m "Tự động commit từ script .bat"
 
-REM Kiểm tra xem có commit mới không
-if %errorlevel% neq 0 (
-    echo Không có thay đổi nào để commit!
-    goto end
-)
-
-REM Đảm bảo rằng không có thay đổi chưa commit
-git stash
-
 REM Pull các thay đổi từ GitLab để đảm bảo không có xung đột
-git pull --rebase origin main
-
-REM Kiểm tra xem có lỗi gì trong quá trình pull không
-if %errorlevel% neq 0 (
-    echo Có lỗi khi pull mã nguồn từ GitLab!
-    exit /b %errorlevel%
-)
-
-REM Khôi phục thay đổi đã stash nếu có
-git stash pop
-
-REM Kiểm tra xem có xung đột khi pop stash không
-if %errorlevel% neq 0 (
-    echo Có xung đột khi khôi phục thay đổi từ stash!
-    exit /b %errorlevel%
-)
+git pull gitlab main --allow-unrelated-histories
 
 REM Đẩy mã nguồn lên GitLab
-git push origin main
-
-REM Kiểm tra xem có lỗi gì trong quá trình push không
-if %errorlevel% neq 0 (
-    echo Đẩy mã nguồn lên GitLab thất bại!
-    exit /b %errorlevel%
-)
+git push gitlab main
 
 echo Đẩy mã nguồn lên GitLab thành công!
-
-:end
 exit
